553052347a77fad328e596abce89c0202867b59a
tanish35 <tanishmajumdar2912@gmail.com>
2024-06-05 10:43:36 +0530

    Minor change

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,12 @@
 import pandas_datareader as pdr
 import pandas as pd
-df=pdr.get_data_tiingo('GOOG',api_key='8eef2d5c370bc76d47b80d774e6f0f6ee507a0a9')
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+api_key = os.getenv('TIINGO_API_KEY')
+df=pdr.get_data_tiingo('GOOG',api_key=api_key)
 df.to_csv('GOOG.csv')
 df=pd.read_csv('GOOG.csv')
 print(df.head())
